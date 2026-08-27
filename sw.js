6155e5d8db4c9c33ffd663028ccd62638b365f56
JBapp-cloud <jakob.bonomo@gmx.at>
2026-08-27 17:50:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Bei jedem App-Update: CACHE_VERSION erhöhen (z.B. v2, v3...)
 // Dadurch wird der alte Cache automatisch gelöscht und die neue Version geladen.
 
-var CACHE_VERSION = "prisma-uebergabe-v20-2.3";
+var CACHE_VERSION = "prisma-uebergabe-v20-2.4";
 
 var ASSETS = [
   "index.html",
